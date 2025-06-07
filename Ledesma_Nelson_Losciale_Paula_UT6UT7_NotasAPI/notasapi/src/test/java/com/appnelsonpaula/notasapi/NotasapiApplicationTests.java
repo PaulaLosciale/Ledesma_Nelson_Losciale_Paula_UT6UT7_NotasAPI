e24a7a1bdd25f08cd35eec3a0ca3504f51d34fd5
Paula <145975341+PaulaLosciale@users.noreply.github.com>
2025-06-07 21:55:58 +0100

    Proyecto base

---
+++ Ledesma_Nelson_Losciale_Paula_UT6UT7_NotasAPI/notasapi/src/test/java/com/appnelsonpaula/notasapi/NotasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appnelsonpaula.notasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
